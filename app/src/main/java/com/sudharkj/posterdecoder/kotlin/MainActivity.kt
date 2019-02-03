5189cc80088985dcd2ec9c5950b3b8f8a35d520d
Ravi <sudha.rkj@gmail.com>
2019-02-03 04:19:41 -0800

    Load image from gallery or using default camera app.
    Resolves poster-decoder/PosterDecoder_Kotlin#2

--- app/src/main/java/com/sudharkj/posterdecoder/kotlin/MainActivity.kt
+++ app/src/main/java/com/sudharkj/posterdecoder/kotlin/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.sudharkj.posterdecoder.kotlin
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +25,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab_gallery.setOnClickListener {
+            dispatchLoadPictureIntent()
+        }
+
+        fab_camera.setOnClickListener {
+            dispatchTakePictureIntent()
         }
     }
 
@@ -34,6 +47,84 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+        const val TAG = "MainActivity" /* Logger tag. */
+        const val REQUEST_IMAGE_LOAD = 1 /* Any integer != -1 is forces for valid request code check */
+        const val REQUEST_IMAGE_CAPTURE = 2 /* Any integer != -1 is forces for valid request code check */
+    }
+
+    private var imageUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if ((requestCode == REQUEST_IMAGE_LOAD || requestCode == REQUEST_IMAGE_CAPTURE) && resultCode == RESULT_OK) {
+            data?.let {
+                if (requestCode == REQUEST_IMAGE_LOAD) {
+                    imageUri = data.data
+                }
+                val imageIntent = Intent(this, ImageActivity::class.java)
+                imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+                startActivity(imageIntent)
+            }
+        }
+    }
+
+    private fun dispatchLoadPictureIntent() {
+        Intent(Intent.ACTION_PICK).also { loadPictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            loadPictureIntent.type = "image/*"
+            startActivityForResult(loadPictureIntent, REQUEST_IMAGE_LOAD)
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File and return null
+                    Log.e(TAG, getString(R.string.error_image_creation), ex)
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        getString(R.string.base_package),
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat(getString(R.string.date_format), Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "${getString(R.string.image_prefix)}_$timeStamp", /* prefix */
+            getString(R.string.image_type), /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Broadcast pic to ACTION_VIEW intents
+            imageUri = toUri()
+            galleryAddPic()
+        }
+    }
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            mediaScanIntent.data = imageUri
+            sendBroadcast(mediaScanIntent)
         }
     }
 }
